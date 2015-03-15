8ee2866d0881e7af2fa8b01dd69ff48b32546d09
Thomas Degry <degry.thomas@gmail.com>
2015-03-14 20:42:40 -0400

    Initial Commit

---
+++ Animation/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Animation
+//
+//  Created by LOANER on 3/14/15.
+//  Copyright (c) 2015 Thomas Degry. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
